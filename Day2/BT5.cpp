e78d23d1c2de740d34d39e0168bf51f40900a15e
Quyet <ducquyetthcs@gmail.com>
2020-08-24 07:30:49 +0700

    Day 2

--- Day2/BT5.cpp
+++ Day2/BT5.cpp
@@ -1,28 +1,63 @@
 #include<iostream>
 using namespace std;
 
+int min(int a, int b){
+    if(a < b){
+        return a;
+    }else{
+        return b;
+    }
+}
+
 int main(){
     int m,n,k;
     freopen("BT5.txt","r",stdin);
     cin>>m>>n>>k;
-    int A[100][100];
+    int A[100][100] = {0};
     int B[100];
     int C[100];
+    // hàng
     for(int i = 0; i < m;i++){
         cin>>B[i];
     }
-    for(int i = 0; i < n;i++){
+    // cột
+    for(int i = 1 ; i <= n;i++){
         cin>>C[i];
     }
     for(int i =0; i < k;i++){
             int a,b;
             cin>>a>>b;
-            A[a-1][b-1] = 1;
+            A[a-1][b] = 100;
     }
-    for(int i =0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            cout<<A[i][j]<<" ";
+    for(int i = 0; i < m;i++){
+        for(int j = 1; j <=n; j++){
+            if(A[i][j] != 0){
+                if(A[i][j] > min(B[i],C[j])){
+                    if(B[i] > C[j]){
+                        A[m][j] = C[j];
+                        for(int a = 0; a < m;a++){
+                            if(A[a][j] > C[j]){
+                                A[a][j] = C[j];
+                            }
+                        }
+                    }else{
+                        A[i][0] = B[i];
+                        for(int a = 1; a <=n;a++){
+                            if(A[i][a] > B[i]){
+                                A[i][a] = B[i];
+                            }
+                        }
+                    }
+                }
+            }
         }
-        cout<<"\n";
     }
+    int t = 0;
+    for(int i = 0; i <m; i++){
+        t+=A[i][0];
+    }
+    for(int i = 1 ; i <=n;i++){
+        t+=A[m][i];
+    }
+    cout<<t;
 }
